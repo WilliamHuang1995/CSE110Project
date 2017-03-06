7ae8e5bc7810065f4a7d038f56ab97d8a8dfcd44
Eric Sen <esen@ucsd.edu>
2017-03-05 19:11:58 -0800

    adding in the urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     url(r'^alex$', views.alex_test, name='alex_test'),
     url(r'^$', views.index, name='index'),
 
-    url(r'^api/get$', views.get_request, name='eric_test')
+    url(r'^api/get$', views.get_request, name='get_request'),
+    url(r'^api/post$', views.post_request, name='posting')
     
 
 ]
