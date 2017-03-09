4ef600a88b2c0ecbeb02ce401090eceac28a9f83
William Huang <wjhuang@ucsd.edu>
2017-03-08 21:47:52 -0800

    everything you change on the calendar, will now affect both calendars (y)

--- webapp/static/webapp/js/home.js
+++ webapp/static/webapp/js/home.js
@@ -8,6 +8,13 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/
 // included, separated by spaces.
 var SCOPES = "https://www.googleapis.com/auth/calendar";
 
+//used for saveChanges()
+var id;
+var changedEvent;
+
+//used for generateEvent()
+var clickedDate;
+
 
 
 
@@ -42,51 +49,48 @@ $(document).ready(function() {
         },
         defaultView: 'month',        
         editable: true,
+        droppable: true, // this allows things to be dropped onto the calendar
+        slotLabelFormat:"HH:mm",
+
+        //When event is resized not on Month view
         eventResize: function(event, delta, revertFunc) {
 
-            alert(event.title + " end is now " + event.end.format());
 
-            if (!confirm("is this okay?")) {
-                revertFunc();
-                return;
-            }
-            console.log(event.start.format('YYYY-MM-DD[T] HH:mm:ss.SSS'));
+            //initialize client
             gapi.client.init({
                 discoveryDocs: DISCOVERY_DOCS,
                 clientId: CLIENT_ID,
                 scope: SCOPES
             });
-            var gevent = {
-                'start': {
-                    'dateTime': event.start.format('YYYY-MM-DD[T]HH:mm:ss.SSS'),
-                    'timeZone': 'America/Los_Angeles'
-                },
-                'end': {
-                    'dateTime': event.end.format('YYYY-MM-DD[T]HH:mm:ss.SSS'),
-                    'timeZone': 'America/Los_Angeles'
-                }
-            };
+            alert(event.title + " end is now " + event.end.format());
+            if (!confirm("is this okay?")) {
+                revertFunc();
+                return;
+            }
+            //If event is a GCal Event.
+            if(event.id!==undefined){
+                var toPushEvent = {
+                    'start': {
+                        'dateTime': event.start.format(),
+                        'timeZone': 'America/Los_Angeles'
+                    },
+                    'end': {
+                        'dateTime': event.end.format(),
+                        'timeZone': 'America/Los_Angeles'
+                    }
+                };  
+                gapi.client.calendar.events.update({
+                    'calendarId': 'primary',
+                    'eventId': event.id,
+                    'resource': toPushEvent
+                }).execute();
+            }
+            //display success message
+            $("#event-add-success").slideDown();
 
-            gapi.client.calendar.events.update({
-                'calendarId': 'primary',
-                'eventId': event.id,
-                'resource': gevent
-            }).execute();
+
         },
-
-
-
-        // this allows things to be dropped onto the calendar
-        droppable: true, 
-        slotLabelFormat:"HH:mm",
-        //if you drop an external event, it removes the original
-        drop: function(event) {
-            $(this).remove();
-            var strSubmitFunc = "addToCalendar()";
-            createModal(event, strSubmitFunc, "Save Changes");
-        },
-        //when you drag'n'drop within calendar 
-        //working as intended.
+        //when you drag'n'drop within calendar, also update GCal 
         eventDrop: function(event, delta, revertFunc) {
             //initialize client
             gapi.client.init({
@@ -112,22 +116,26 @@ $(document).ready(function() {
                     'resource': toPushEvent
                 }).execute();
             }
-            alert("succ");
+            //display success message
+            $("#event-add-success").slideDown();
 
         },
-
-
+        //if you drop an external event, it removes the original
+        drop: function(event) {
+            $(this).remove();
+            changedEvent=event;
+            createModal(event, "addToCalendar()", "Add To Calendar");
+        },
         //when you click on the day.
-        dayClick: function(date, jsEvent, view) {            
+        dayClick: function(date, jsEvent, view) {              //easter egg
             if(date.format("MM:DD")==="04:20"){
-                //easter egg
+
                 var win = window.open("https://www.youtube.com/watch?v=XtECttp9WUk", '_blank');
                 win.focus();
             } 
-            createModal(undefined, "createEvent", "Create Event")
-
-
-
+            var strSubmitFunc = "generateEvent()";
+            clickedDate = date;
+            createModal(undefined, strSubmitFunc, "Create Event")
         },
 
         //By Daniel Keirouz. When you click on an event. 
@@ -149,66 +157,205 @@ $(document).ready(function() {
     });
 });
 
-//googleCalendarId and changedEvent
-var id;
-var changedEvent;
 
+/**
+ * By William
+ * Trigger: User wants to create an event and clicks on Day
+ * Precondition: No events exist, no field is filled
+ * Postcondition: fields are filled, event pushed to GCal (optional?)
+ */
+
+function generateEvent(){
+    alert("generateEvent");
+    try{
+        //Since no event exist prior, no need to update event. But need to create event to add to calendar
+
+        var calendoEvent = {
+            title: $('#event-name-input').val(), 
+            start: $('#start-time-input').val(),
+            end: $('#end-time-input').val(),
+            location: $('#location-input').val(),
+            description: $('#description-input').val()
+        }
+        //render event
+        $('#calendar').fullCalendar('renderEvent', calendoEvent,stick=true);
+        var gCalEvent = {
+            'summary': $('#event-name-input').val(),
+            'location': $('#location-input').val(),
+            'description': $('#description-input').val(),
+            'start': {
+                'dateTime': $('#start-time-input').val()+':00.00',
+                'timeZone': 'America/Los_Angeles'
+            },
+            'end': {
+                'dateTime': $('#end-time-input').val()+':00.00'            ,
+                'timeZone': 'America/Los_Angeles'
+            },
+            'reminders': {
+                'useDefault': false,
+                'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10}
+                ]
+            }
+        };  
+
+        //initialize client
+        gapi.client.init({
+            discoveryDocs: DISCOVERY_DOCS,
+            clientId: CLIENT_ID,
+            scope: SCOPES
+        });
+
+        gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': gCalEvent
+        }).execute();
+        
+        //close the modal window after completion
+        $("#modalWindow").modal('hide');
+        
+        //succuess notification
+        $("#event-add-success").slideDown();
+    }catch(e){
+        console.log(e);
+        $("#modalWindow").modal('hide');
+        $("#event-add-failure").slideDown();
+    }
+}
+
+/**
+ * By William
+ * Trigger: An event reside on the To Do list.
+ * Precondition: event already exist but not on calendar
+ * Postcondition: event is displayed on calendar and also pushed to GCal (optional?)
+ */
+function addToCalendar(){
+    console.log(changedEvent.title);
+    alert("addToCalendar");
+    try{
+        //save local title, start, end time.
+        changedEvent.start=$('#start-time-input').val();
+        changedEvent.end=$('#end-time-input').val();
+        var newTitle = $('#event-name-input').val();
+        changedEvent.title = newTitle==""?"(No Title)":newTitle;
+        //save local description
+        changedEvent.description = $('#description-input').val()
+        //save local location
+        changedEvent.location = $('#location-input').val()
+        //add event
+        $('#calendar').fullCalendar('addEventSource', changedEvent);
+        //render event not sure if I need both
+        $('#calendar').fullCalendar('renderEvent', changedEvent,stick=true);
+        var event = {
+            'summary': $('#event-name-input').val(),
+            'location': $('#location-input').val(),
+            'description': $('#description-input').val(),
+            'start': {
+                'dateTime': $('#start-time-input').val()+':00.00',
+                'timeZone': 'America/Los_Angeles'
+            },
+            'end': {
+                'dateTime': $('#end-time-input').val()+':00.00'            ,
+                'timeZone': 'America/Los_Angeles'
+            },
+            'reminders': {
+                'useDefault': false,
+                'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10}
+                ]
+            }
+        };  
+        //initialize client
+        gapi.client.init({
+            discoveryDocs: DISCOVERY_DOCS,
+            clientId: CLIENT_ID,
+            scope: SCOPES
+        });
+
+        gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event
+        }).execute();
+        //close the modal window after completion
+        $("#modalWindow").modal('hide');
+
+        //success notification
+        $("#event-add-success").slideDown();
+    }catch(e){
+        console.log(e);
+        $("#modalWindow").modal('hide');
+        $("#event-add-failure").slideDown();
+    }
+}
 /** 
 * Function is only called when event exists on Calendar
 * The event can either be a Calendo Event or a Google Calendar Event.
 * The only difference would be that we refrain from assigning an id to Calendo Events as the ID is how we actually push Calendo Events to Google Calendar.
 */
 function saveChanges() {
-    //save local title, start, end time.
-    changedEvent.start=$('#start-time-input').val();
-    changedEvent.end=$('#end-time-input').val();
-    var newTitle = $('#event-name-input').val();
-    changedEvent.title = newTitle==""?"(No Title)":newTitle;
-    //save local description
-    changedEvent.description = $('#description-input').val()
-    //save local location
-    changedEvent.location = $('#location-input').val()
+    try{
+        //save local title, start, end time.
+        changedEvent.start=$('#start-time-input').val();
+        changedEvent.end=$('#end-time-input').val();
+        var newTitle = $('#event-name-input').val();
+        changedEvent.title = newTitle==""?"(No Title)":newTitle;
+        //save local description
+        changedEvent.description = $('#description-input').val()
+        //save local location
+        changedEvent.location = $('#location-input').val()
 
-    //update event
-    $('#calendar').fullCalendar('updateEvent', changedEvent);
+        //update event
+        $('#calendar').fullCalendar('updateEvent', changedEvent);
 
-    //initialize client
-    gapi.client.init({
-        discoveryDocs: DISCOVERY_DOCS,
-        clientId: CLIENT_ID,
-        scope: SCOPES
-    });
+        var event = {
+            'summary': $('#event-name-input').val(),
+            'location': $('#location-input').val(),
+            'description': $('#description-input').val(),
+            'start': {
+                'dateTime': $('#start-time-input').val()+':00.00',
+                'timeZone': 'America/Los_Angeles'
+            },
+            'end': {
+                'dateTime': $('#end-time-input').val()+':00.00'            ,
+                'timeZone': 'America/Los_Angeles'
+            },
+            'reminders': {
+                'useDefault': false,
+                'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10}
+                ]
+            }
+        };
+        //initialize client
+        gapi.client.init({
+            discoveryDocs: DISCOVERY_DOCS,
+            clientId: CLIENT_ID,
+            scope: SCOPES
+        });
 
-    var event = {
-        'summary': $('#event-name-input').val(),
-        'location': $('#location-input').val(),
-        'description': $('#description-input').val(),
-        'start': {
-            'dateTime': $('#start-time-input').val()+':00.00',
-            'timeZone': 'America/Los_Angeles'
-        },
-        'end': {
-            'dateTime': $('#end-time-input').val()+':00.00'            ,
-            'timeZone': 'America/Los_Angeles'
-        },
-        'reminders': {
-            'useDefault': false,
-            'overrides': [
-                {'method': 'email', 'minutes': 24 * 60},
-                {'method': 'popup', 'minutes': 10}
-            ]
-        }
-    };  
-    gapi.client.calendar.events.update({
-        'calendarId': 'primary',
-        'eventId':id,
-        'resource': event
-    }).execute();
-    //close the modal window after completion
-    $("#modalWindow").modal('hide');
-    //display success message
-    $("#event-add-success").slideDown();
+        gapi.client.calendar.events.update({
+            'calendarId': 'primary',
+            'eventId':id,
+            'resource': event
+        }).execute();
+        //close the modal window after completion
+        $("#modalWindow").modal('hide');
+        //display success message
+        $("#event-add-success").slideDown();
+    }catch(e){
+        console.log(e);
+        $("#modalWindow").modal('hide');
+        $("#event-add-failure").slideDown();
+    }
 }
+
+/*
+ * Deletes the Event from Calendo and GCal
+ * It does both since most people wouldn't migrate here if it is not synchronized
+ */
 function deleteEvent(){
     //TODO
     $("#modalWindow").modal('hide');
@@ -224,7 +371,17 @@ function hide(){
     $("#event-add-failure").slideUp();
 }
 
+/*
+ * Created by Daniel Keirouz
+ * Enhanced by William Huang
+ * Description: This should technically no longer be
+ * called createModal as the function assigns value to
+ * different field and displays it
+ */
 function createModal(calEvent, strSubmitFunc, eventType) {
+    //might wanna autofill the clickedDate for ease of access
+    //console.log(clickedDate.format());
+
     //Creating a Calendo Event
     if (eventType==="Create Event"){
         $('h4.eventType').text('Create Event');
@@ -237,6 +394,8 @@ function createModal(calEvent, strSubmitFunc, eventType) {
         $('#end-time-input').val('');
         $('#location-input').val('');
         $('#description-input').val('');
+        $(".confirmation-button").attr("onclick",strSubmitFunc);
+        $("#url").hide();
     } else{
         //Calendo Event
         if(calEvent.id==undefined){
@@ -251,6 +410,8 @@ function createModal(calEvent, strSubmitFunc, eventType) {
             $('#end-time-input').val('');
             $('#location-input').val(calEvent.location);
             $('#description-input').val(calEvent.description);
+            $(".confirmation-button").attr("onclick",strSubmitFunc);
+            $("#url").hide();
         }
         //Google Calendar Event
         else{
@@ -267,6 +428,10 @@ function createModal(calEvent, strSubmitFunc, eventType) {
             $('#end-time-input').val(calEvent.end.format('YYYY-MM-DD[T]HH:mm'));
             $('#location-input').val(calEvent.location);
             $('#description-input').val(calEvent.description);
+            $(".confirmation-button").attr("onclick",strSubmitFunc);
+            console.log(calEvent.url);
+            $("#url").attr("href",""+calEvent.url);
+            $("#url").show();
         }
     }
 
