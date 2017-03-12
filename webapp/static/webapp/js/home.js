4905483847de11e7a919ca7866677dd7106281df
Lily Tang <lilytang@lilys-mbp-3.dynamic.ucsd.edu>
2017-03-12 04:20:13 -0700

    stuff

--- webapp/static/webapp/js/home.js
+++ webapp/static/webapp/js/home.js
@@ -553,4 +553,9 @@ function saveChanges() {
 
 
 
-
+	  $('input').blur(function() {
+    if ($(this).val())
+      $("label[for='"+$(this).attr('id')+"']").addClass('used');
+    else
+      $("label[for='"+$(this).attr('id')+"']").removeClass('used');
+  });
