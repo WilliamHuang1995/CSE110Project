fac9bbcee77aaa3b8026aedacc8851fa84d81707
Alex Garcia <alexsebastian.garcia@gmail.com>
2017-03-13 02:53:57 -0700

    fuck

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,6 +143,6 @@ EMAIL_USE_SSL = True
 CSRF_USE_SESSIONS = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-#X_FRAME_OPTIONS = 'ALLOWALL'
+X_FRAME_OPTIONS = 'DENY'
 SESSION_COOKIE_DOMAIN = "www.calen-do.com"
 #CORS_ORIGIN_ALLOW_ALL = true 
