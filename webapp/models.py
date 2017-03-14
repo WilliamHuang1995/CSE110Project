13944b3bb073c0cfea00faa7de385ae2a3853793
Eric Sen <esen@ucsd.edu>
2017-03-13 23:41:07 -0700

    removing user

--- webapp/models.py
+++ webapp/models.py
@@ -13,20 +13,6 @@ class Session(models.Model):
 	def __str__(self):
 		return self.SessionId
 
-class User(models.Model):
-	VIEW_CHOICES = (
-		('A', 'Agenda'),
-		('T', 'Three-Day'),
-		('M', 'Month')
-	)
-	email = models.CharField(max_length=30)
-	Password = models.CharField(max_length=20)
-	NotificationsFreq = models.BooleanField(default=False)
-	ColorScheme = models.IntegerField()
-	CalenderView = models.CharField(max_length=1, choices=VIEW_CHOICES)
-
-	def __str__(self):
-			return self.UserName
 
 class Calendo_User(models.Model):
 	VIEW_CHOICES = (
@@ -65,7 +51,7 @@ class Todo(models.Model):
 	#need int field for isscheduled 
 	#need int field for smart schedule 
 	#SmartSchedule = models.IntegerField(default=0)
-
+	#IsScheduled = models.IntegerField(default=0)
 	
 
 	def __str__(self):
