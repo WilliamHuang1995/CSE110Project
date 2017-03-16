9ebf2ee30f1c2c3b5404a28633df62e162442b91
h1kapoor <h1kapoor@ucsd.edu>
2017-03-15 21:23:10 -0700

    Read existing todos

--- webapp/static/webapp/todo.js
+++ webapp/static/webapp/todo.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     else
       $("label[for='"+$(this).attr('id')+"']").removeClass('used');
   });
-  
+
 });
 
 function showExtraSettings() {
@@ -14,52 +14,73 @@ function showExtraSettings() {
 	$('#smart-schedule-priority').slideToggle();
 }
 
-/*function getRequest(){
-
-    $(document).ready(function(){
-        var client = new HttpClient();
-        client.get('/api/get', function(response) {
-        // do something with response
-            var temp = JSON.parse(response); 
-
-            console.log(temp[0].title);
-            console.log(temp.length);    
-            document.getElementById("task").value = temp.title;
-            document.getElementById("loc").value = temp.location;
-            document.getElementById("desc").value = temp.description;
-            document.getElementById('example-date-input').value = temp.dueDate;
-
-            var estimatedTime = inputHours * 60 + inputMins;
-            inputHours = (Number.parseInt(temp.estimatedTime)/60).toString(); 
-            inputMins = (Number.parseInt(temp.estimatedTime)%60).toString(); 
-
-            document.getElementById("numHours").value = inputHours;
-            document.getElementById("numMins").value = inputMins;
-
-
-
-
-            
-        });
-    }); 
+function getRequest(id){
     var HttpClient = function() {
     this.get = function(aUrl, aCallback) {
         var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-            }
+        anHttpRequest.onreadystatechange = function() {
+          if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+              aCallback(anHttpRequest.responseText);
+          }
 
-            anHttpRequest.open( "GET", aUrl, true );            
-            anHttpRequest.send( null );
+          anHttpRequest.open( "GET", aUrl, true );
+          anHttpRequest.send( null );
         }
     }
-}*/
-//trying a callback post request :) 
+
+    $(document).ready(function(){
+        var client = new HttpClient();
+        client.get('/api/get?id='+id, function(response) {
+        // do something with response
+            var temp = JSON.parse(response);
+            temp = temp[0];
+            document.getElementById("task").value = temp.title;
+            $('#task').focus();
+            document.getElementById("loc").value = temp.location;
+            $('#loc').focus();
+            document.getElementById("desc").value = temp.description;
+            $('#desc').focus();
+            document.getElementById('example-date-input').value = temp.year + "-" + temp.month + "-" + temp.day;
+
+            //$('#smart-schedule-switch').trigger('click');
+            //$.getScript("https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js")
+            //$('#smart-schedule-switch').bootstrapToggle('toggle')
+            $('#smart-schedule-switch').trigger('click');
+            //$('#smart-schedule-switch').bootstrapToggle('on')
+            //test = document.getElementById("smart-schedule-switch");
+            //test.bootstrapToggle('toggle');
+            //$('#smart-schedule-switch').prop('checked', true).change()
+            //toggleOnByInput()
+            //demo.toggle('#smart-schedule-switch')
+
+            $('#desc').blur();
+
+            inputHours = Math.floor((Number.parseInt(temp.estimateTime)/60)).toString();
+            inputMins = (Number.parseInt(temp.estimateTime)%60).toString();
+
+            if (inputHours == 1){
+              document.getElementById("numHours").value = inputHours + " hour";
+            }
+            else{
+              document.getElementById("numHours").value = inputHours + " hours";
+            }
+
+            document.getElementById("numMins").value = inputMins + " min";
+            document.getElementById("priorityOpt").value = "High"; //temp.priority
+
+
+
+
+
+        });
+    });
+
+}
+//trying a callback post request :)
 
 
 function postRequest(){
-     
+
     var HttpClient = function() {
         this.post = function(aUrl, aCallback) {
             var inputTitle = document.getElementById("task").value;
@@ -71,42 +92,42 @@ function postRequest(){
 
             inputHours = Number.parseInt(inputHours.split(' ')[0]);
             inputMins = Number.parseInt(inputMins.split(' ')[0]);
-                    
+
             var estimatedTime = inputHours * 60 + inputMins;
-						
+
 						if(Number.isNaN(estimatedTime))
 							estimatedTime = 0;
 
 
             var inputPriority = document.getElementById("priorityOpt").value;
-            var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val(); 
+            var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
             console.log("crsf token"+ csrftoken);
             var url = "/api/post";
             var form_data = new FormData();
-                
+
             form_data.append("title", inputTitle);
             form_data.append("location", inputLoc);
-            form_data.append("description", inputDescrip); 
+            form_data.append("description", inputDescrip);
             form_data.append("dueDate", inputDate);
             form_data.append("estimateTime", estimatedTime);
             form_data.append("priority", inputPriority);
 
 
             var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function() { 
+            anHttpRequest.onreadystatechange = function() {
                 if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
                     aCallback(anHttpRequest.responseText);
             }
-                
 
 
-                
+
+
             //Send the proper header information along with the request
             //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    
 
-            anHttpRequest.open( "POST", aUrl, true );   
-            anHttpRequest.setRequestHeader("X-CSRFToken", csrftoken);         
+
+            anHttpRequest.open( "POST", aUrl, true );
+            anHttpRequest.setRequestHeader("X-CSRFToken", csrftoken);
             anHttpRequest.send( form_data );
             console.log("sent params");
         }
@@ -114,9 +135,9 @@ function postRequest(){
     var client = new HttpClient();
     client.post('/api/post', function(response) {
         // do something with response
-        var temp = JSON.parse(response); 
-        console.log(temp); 
-        
+        var temp = JSON.parse(response);
+        console.log(temp);
+
 
         $("#unscheduledDiv").append('\
             <a href="#" class="list-group-item list-group-item-action list-group-item-unscheduled">\
@@ -128,22 +149,22 @@ function postRequest(){
                     </p>\
                 </div>\
             </a>');
-            
+
         document.getElementById("task").value = null;
         document.getElementById("loc").value = null;
         document.getElementById("desc").value = null;
         document.getElementById('example-date-input').value = "";
         document.getElementById("numHours").value = "";
         document.getElementById("numMins").value = "";
-        
+
     });
 }
 // Create a new list item when clicking on the "Add" button
 function newTodo() {
     //if statement checking if task has id unsure since it has an id as well
-    //then either edit or post depending on if it has id 
+    //then either edit or post depending on if it has id
     postRequest();
-    
+
 
         //adding to scheduled div
     //This may be bad practice
@@ -157,8 +178,8 @@ function newTodo() {
     //window.location.reload(true);
         */
 
-}; 
- 
+};
+
 
 /*function postRequest(){
     var inputTitle = document.getElementById("task").value;
@@ -174,19 +195,19 @@ function newTodo() {
 
 		inputHours = Number.parseInt(inputHours.split(' ')[0]);
 		inputMins = Number.parseInt(inputMins.split(' ')[0]);
-		
+
 		var estimatedTime = inputHours * 60 + inputMins;
 
 
     var inputPriority = document.getElementById("priorityOpt").value;
-    var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val(); 
+    var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
 		console.log("crsf token"+ csrftoken);
     var url = "/api/post";
 		var form_data = new FormData();
-    
+
 		form_data.append("title", inputTitle);
 		form_data.append("location", inputLoc);
-		form_data.append("description", inputDescrip); 
+		form_data.append("description", inputDescrip);
 		form_data.append("dueDate", inputDate);
 		form_data.append("estimateTime", estimatedTime);
 		form_data.append("priority", inputPriority);
@@ -194,7 +215,7 @@ function newTodo() {
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
-                
+
             //Send the proper header information along with the request
             //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.setRequestHeader("X-CSRFToken", csrftoken);
@@ -202,13 +223,13 @@ function newTodo() {
     xhr.send(form_data);
     console.log("sent params");
 
-   /* var json_data = xhr.responseText;  
+   /* var json_data = xhr.responseText;
     alert(json_data);
     console.log(typeof(json_data));
     console.log(json_data);
     var temp = JSON.parse(json_data);
-    console.log(temp[0].id); 
-    return temp;    
+    console.log(temp[0].id);
+    return temp;
     */
 /*
 };
@@ -218,41 +239,41 @@ function checkTodo(){
     var i;
     for (i = 0; i < check.length; i++){
         check[i].onclick = function(){
-            var div = this; 
-            console.log(div); 
+            var div = this;
+            console.log(div);
             var text = div.getAttribute('id');
-            console.log(text); 
+            console.log(text);
 
-            checkRequest(text); 
-            div.parentElement.style.display = "::after"; 
+            checkRequest(text);
+            div.parentElement.style.display = "::after";
         }
     }
 }
 
 function checkRequest(someValue){
-    var inputValue = someValue; 
+    var inputValue = someValue;
     var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
     var url = "/api/click";
 
-    var params = inputValue; 
-    var form_data = new FormData(); 
+    var params = inputValue;
+    var form_data = new FormData();
     form_data.append("id", params);
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
-                
+
             //Send the proper header information along with the request
             //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.setRequestHeader("X-CSRFToken", csrftoken);
 
     xhr.send(form_data);
-    console.log("sent params");  
+    console.log("sent params");
 
 }
 
 
 
-//should execute on delete call 
+//should execute on delete call
 function deleteTodo(){
     var close = document.getElementsByClassName("glyphicon glyphicon-trash glyphicon-trash-u");
     var i;
@@ -260,34 +281,33 @@ function deleteTodo(){
       close[i].onclick = function() {
         var div = this;
         console.log(div);
-        //var text = div["innerText" in div ? "innerText" : "textContent"]; 
-        var text = div.getAttribute('id'); 
+        //var text = div["innerText" in div ? "innerText" : "textContent"];
+        var text = div.getAttribute('id');
         console.log(text);
 
-        deleteRequest(text);   
+        deleteRequest(text);
         div.parentElement.style.display = "none";
       }
-    }    
+    }
 };
 
 
 function deleteRequest(someValue){
     var inputValue = someValue;
-    var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val(); 
+    var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
     var url = "/api/delete";
-    
+
     var params = inputValue;
     var form_data = new FormData();
-    form_data.append("id", params); 
-    
+    form_data.append("id", params);
+
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
-                
+
             //Send the proper header information along with the request
             //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.setRequestHeader("X-CSRFToken", csrftoken);
 
     xhr.send(form_data);
-    console.log("sent params");    
+    console.log("sent params");
 }
-    
--- webapp/views/viewsapi.py
+++ webapp/views/viewsapi.py
@@ -12,9 +12,9 @@ from ..models import Calendo_User
 import json
 import time
 import math
-from .viewslogin import * 
+from .viewslogin import *
 from .viewshtml import *
-from .viewstest import * 
+from .viewstest import *
 from .userAuth import *
 
 from ..models import Confirm_Email
@@ -35,43 +35,53 @@ def get_request(request):
 	#db_result = Todo.objects.raw('SELECT * FROM webapp_todo')
 	#db_json = serializers.serialize('json', db_result, fields=('id', 'title'))
 	#return JsonResponse(db_json, safe=False)
-	queryset = Todo.objects.raw('SELECT * FROM webapp_todo WHERE id = %s', request.GET.get('id'))
-	data = [{'title': item.title, 'description': item.Description, 'estimateTime': item.EstimateTime, 'dueDate': item.dueDate, 'location': item.location} for item in queryset]
+	queryset = Todo.objects.raw('SELECT * FROM webapp_todo WHERE id = %s', [request.GET.get('id')])
+
+	data = []
+
+	for item in queryset:
+		x = str(item.DueDate)
+		year = x[0:4]
+		month = x[5:7]
+		day = x[8:10]
+		todoTuple = {'title': 	item.title, 'description': item.Description, 'estimateTime': item.EstimateTime, 'year': year, 'month': month, 'day': day, 'location': item.Location, 'isChecked': item.IsChecked}
+		data.append(todoTuple)
+		
 	return HttpResponse(json.dumps(data),content_type='application/json')
 
 def update_request(request):
-	if request.method != 'POST': 
+	if request.method != 'POST':
 		return redirect('/')
-	
+
 	userAuth = user_is_auth(request)
 	if not userAuth:
 		return prompt_login(request)
 
-	
 
-	
+
+
 	edit_id = request.POST.get('id')
 	print(edit_id)
 	insertToDoResult = Todo(id=edit_id, IsScheduled = 1)
-	
-	insertToDoResult.save(update_fields=['IsScheduled']) 
+
+	insertToDoResult.save(update_fields=['IsScheduled'])
 
 	return render(request, 'webapp/todo-test.html')
 
 def check_request(request):
-	if request.method != 'POST': 
+	if request.method != 'POST':
 		return redirect('/')
-	
+
 	userAuth = user_is_auth(request)
 	if not userAuth:
 		return prompt_login(request)
 
-	
+
 	edit_id = request.POST.get('id')
 	print(edit_id)
 	insertToDoResult = Todo(id=edit_id, IsChecked = 1)
-	
-	insertToDoResult.save(update_fields=['IsChecked']) 
+
+	insertToDoResult.save(update_fields=['IsChecked'])
 
 	return render(request, 'webapp/todo-test.html')
 
@@ -79,30 +89,30 @@ def check_request(request):
 def post_request(request):
 	if request.method != 'POST':
 			return redirect('/todo')
-	
+
 	userAuth = user_is_auth(request)
 	if not userAuth:
 		return prompt_login(request)
 
 	#see if all were provided
 	if( not (request.POST.get('title') )):
-		
+
 		#TODO error handling, give them error messages
 		return redirect('/home')
 
-	
+
 	input_title = request.POST.get('title')
 	input_description = request.POST.get('description')
 	input_estimatedTime = request.POST.get('estimateTime')
 	input_priority = request.POST.get('priority')
 	input_dueDate = request.POST.get('dueDate')
 	input_location = request.POST.get('location')
-	
+
 	print(type(input_estimatedTime))
 	#if type(input_estimatedTime) == str or math.isnan(input_estimatedTime):
 #		print("asdfasdf")
 #		input_estimatedTime = 0
-	
+
 	insertToDoResult = Todo(title=input_title,UserID=userAuth,Description=input_description,EstimateTime=input_estimatedTime,DueDate=input_dueDate, Location=input_location)
 	insertToDoResult.save()
 
@@ -114,25 +124,25 @@ def post_request(request):
 def edit_request(request):
 	if request.method != 'POST':
 			return redirect('/todo')
-	
+
 	userAuth = user_is_auth(request)
 	if not userAuth:
 		return prompt_login(request)
 
 	#see if all were provided
 	if( not (request.POST.get('title') )):
-		
+
 		#TODO error handling, give them error messages
 		return redirect('/home')
 
-	
+
 	input_title = request.POST.get('title')
 	input_description = request.POST.get('description')
 	input_estimatedTime = request.POST.get('estimateTime')
 	input_priority = request.POST.get('priority')
 	input_dueDate = request.POST.get('dueDate')
 	input_location = request.POST.get('location')
-	
+
 	edit_id = request.POST.get('id')
 	#insertToDoResult = Todo(title=input_title,UserID=userAuth,Description=input_description,EstimateTime=input_estimatedTime,DueDate=input_dueDate, Location=input_location)
 	insertToDoResult = Todo.objects.filter(id=edit_id)
@@ -140,7 +150,7 @@ def edit_request(request):
 	insertToDoResult.Description = input_description
 	insertToDoResult.EstimateTime = input_estimatedTime
 	insertToDoResult.DueDate = input_dueDate
-	insertToDoResult.Location = input_location 
+	insertToDoResult.Location = input_location
 	insertToDoResult.save()
 
 	#queryset = Todo.objects.raw('SELECT id FROM webapp_todo WHERE UserID=%s',[calendo_session_token])
@@ -155,17 +165,14 @@ def delete_request(request):
 	print("dropping")
 
 	if( not (request.POST.get('id') )):
-		
+
 		#TODO error handling, give them error messages
 		return redirect('/home')
 
-	#this might need to be changed to id 
+	#this might need to be changed to id
 	delete_id = request.POST.get('id')
 
 	#deleteQuery = Todo.objects.raw('DELETE FROM webapp_todo WHERE "title" = %s', [delete_title])
-	Todo.objects.filter(id=delete_id).delete() 
+	Todo.objects.filter(id=delete_id).delete()
 
 	return render(request, 'webapp/todo.html')
-
-
-
